d3d96f61f81151d234d86447ee2f250b140761f6
Konstantin Kiritsenko <k@componentk.com>
2018-01-17 18:26:35 +0800

    WEBC-449 Using constants for response codes
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- src/ValueObject/Response.php
+++ src/ValueObject/Response.php
@@ -23,6 +23,15 @@ namespace Shopgate\CloudIntegrationSdk\ValueObject;
 
 class Response
 {
+    const HTTP_OK                    = 200;
+    const HTTP_BAD_REQUEST           = 400;
+    const HTTP_UNAUTHORIZED          = 401;
+    const HTTP_FORBIDDEN             = 403;
+    const HTTP_NOT_FOUND             = 404;
+    const HTTP_METHOD_NOT_ALLOWED    = 405;
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
+    const HTTP_NOT_IMPLEMENTED       = 501;
+
     /** @var int */
     private $code;
 
@@ -52,14 +61,16 @@ class Response
     /**
      * @return int
      */
-    public function getCode() {
+    public function getCode()
+    {
         return $this->code;
     }
 
     /**
      * @return string[]
      */
-    public function getHeaders() {
+    public function getHeaders()
+    {
         return $this->headers;
     }
 
@@ -79,7 +90,8 @@ class Response
     /**
      * @return string
      */
-    public function getBody() {
+    public function getBody()
+    {
         return $this->body;
     }
 }

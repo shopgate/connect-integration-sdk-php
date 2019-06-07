445c1361d7a8dff9683d740a825e9a97210eaacb
Timo Ebel <timo.ebel@gmail.com>
2019-06-07 21:44:19 +0200

    Added some console logging for the Google PubSub setup.

--- tools/pubsubfiller.php
+++ tools/pubsubfiller.php
@@ -15,31 +15,39 @@ $subscriptionCreated = false;
 $tries = 0;
 while (!$topic && !$subscriptionCreated && $tries < 10) {
     try {
+        echo 'Creating Google PubSub topic "entityChanged" . . . ';
         $topic = $pubSub->createTopic('entityChanged');
         $topicCreated = true;
+        echo "[OK]\n";
     } catch (Exception $e) {
         if ($e->getCode() === 409) {
+            echo "[OK] Already exists.\n";
             $topic = $pubSub->topic('entityChanged');
         }
     }
 
     if (!$topic) {
+        echo "[FAILED] Retrying in 1s.\n";
         $tries++;
         sleep(1);
         continue;
     }
 
     try {
+        echo 'Creating Google PubSub subscription "workerScvEntityChanged" to topic "entityChanged" . . . ';
         $pubSub->subscribe('workerSvcEntityChanged', 'entityChanged');
         $subscriptionCreated = true;
+        echo "[OK]\n";
         continue;
     } catch (Exception $e) {
         if ($e->getCode() === 409) {
+            echo "[OK] Already exists.\n";
             $subscriptionCreated = true;
             continue;
         }
     }
 
+    echo "[FAILED] Retrying in 1s.\n";
     $tries++;
     sleep(1);
 }

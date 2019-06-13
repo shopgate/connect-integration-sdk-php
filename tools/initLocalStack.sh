919e7a30096b81d42a7789f989c1cd5357132168
Timo Ebel <timo.ebel@gmail.com>
2019-06-13 17:46:20 +0200

    Cleanup of initLocalStack.sh; added a docker-composer.dev.yml for local stack (includes volume mounting & sync); added booting up CI with no mounts; exposed ports now configurable via .env or env variables when running on local stack (not CI).

--- tools/initLocalStack.sh
+++ tools/initLocalStack.sh
@@ -20,22 +20,11 @@ function retry {
     echo "$1 ready"
 }
 
-BUILD="build\: \."
-pipe="> docker-compose.yml"
+CI_STACK=${CI_STACK:-}
 DOCKER_COMPOSE_FILES="-f docker-compose.yml"
-if [ -n "$TEST_IMAGE" ]; then
-    TEST_IMAGE=${TEST_IMAGE//:/\\:}
-    BUILD="image\: ${TEST_IMAGE}"
-    DOCKER_COMPOSE_FILES="-f docker-compose.yml"
+if [[ -z "$CI_STACK" ]]; then
+    DOCKER_COMPOSE_FILES="-f docker-compose.yml -f docker-compose.dev.yml"
 fi
-
-if [[ ! -z "$EXPOSED_PORT_MYSQL" ]]; then
-    sh -c "sed -e \"s:%MYSQL_PORTS%:ports\:\n      - \$\{EXPOSED_PORT_MYSQL\:-3306\}\:3306:g\" docker-compose.yml.tpl > docker-compose.yml.tmp"
-else
-    sh -c "sed -e \"s:%MYSQL_PORTS%::g\" docker-compose.yml.tpl > docker-compose.yml.tmp"
-fi
-
-sh -c "sed -e \"s:%BUILD%:$BUILD:g\" docker-compose.yml.tmp $pipe"
 
 export SERVICE=sdk
 
@@ -51,8 +40,6 @@ docker-compose $DOCKER_COMPOSE_FILES up -d mysql
 docker-compose $DOCKER_COMPOSE_FILES up -d etcd
 docker-compose $DOCKER_COMPOSE_FILES up -d googlepubsub-emulator
 
-docker-compose exec -T php pwd
-docker-compose exec -T php ls -al
 docker-compose exec -T php composer update
 docker-compose exec -T php php ./tools/pubsubfiller.php
 docker-compose exec -T php php ./tools/etcdfiller.php

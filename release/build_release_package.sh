8e940f8b95afdd8e3ffebef0a0b6d35bc2423263
Timo Ebel <timo.ebel@gmail.com>
2019-06-14 16:35:28 +0200

    Fixed release package build failing due to unmet platform requirements.

--- release/build_release_package.sh
+++ release/build_release_package.sh
@@ -2,7 +2,7 @@
 
 mkdir release/shopgate-cloud-integration-sdk-php
 rm -rf vendor
-composer install -vvv --no-dev
+composer install -vvv --no-dev --ignore-platform-reqs
 rsync -av --exclude-from './release/exclude-filelist.txt' ./ release/shopgate-cloud-integration-sdk-php
 cd release
 zip -r ../shopgate-cloud-integration-sdk-php.zip shopgate-cloud-integration-sdk-php

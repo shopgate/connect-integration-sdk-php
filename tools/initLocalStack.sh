80332f9ce11d1ab81c62cbfd0b0b839941a5794d
Alexander Wesselburg <info@myportal.de>
2019-07-11 13:13:30 +0200

    MAGENTO-304 - init omni-customer

--- tools/initLocalStack.sh
+++ tools/initLocalStack.sh
@@ -61,7 +61,7 @@ retry "CatalogService" "docker-compose exec -T catalog curl http://localhost/hea
 docker-compose $DOCKER_COMPOSE_FILES up -d elasticsearch
 retry "elasticsearch" "docker-compose exec -T elasticsearch curl http://localhost:9200/_cluster/health?wait_for_status=yellow 2>&1"
 
-docker-compose $DOCKER_COMPOSE_FILES up -d mysql auth redis omni-worker omni-event-receiver omni-merchant omni-location s3 import
+docker-compose $DOCKER_COMPOSE_FILES up -d mysql auth redis omni-worker omni-event-receiver omni-merchant omni-location s3 import omni-customer
 
 retry "AuthService" "docker-compose exec -T auth curl http://localhost/health -o /dev/null 2>&1"
 retry "MerchantService" "docker-compose exec -T omni-merchant curl http://localhost/health -o /dev/null 2>&1"

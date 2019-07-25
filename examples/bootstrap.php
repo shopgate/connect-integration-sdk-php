f479b4d65d2989df79ae2d4741abf35f10c1978f
Oliver Müller <oliver.mueller@shopgate.com>
2019-07-25 10:30:41 +0200

    CIS-36: refactored config for examples

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -5,11 +5,14 @@ use Shopgate\ConnectSdk\ShopgateSdk;
 require dirname(__FILE__) . '/../vendor/autoload.php';
 require dirname(__FILE__) . '/fixtures.php';
 
-$config = [
-    'merchantCode'  => 'BANA',
-    'clientId'      => 'xxx',
-    'clientSecret'  => 'xxx',
-    'env'           => 'dev'
-];
+if (file_exists(dirname(__FILE__) . '/config.php')) {
+    require_once dirname(__FILE__) . '/config.php';
+} else {
+    $config = [
+        'merchantCode'  => 'xxx',
+        'clientId'      => 'xxx',
+        'clientSecret'  => 'xxx',
+    ];
+}
 
 $sdk = new ShopgateSdk($config);

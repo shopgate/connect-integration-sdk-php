ba39cf1522ccff55c9bb3c5e308e14b2dab4d5f1
Aaron Martin <aaron.martin@shopgate.com>
2019-07-30 11:17:59 -0500

    CIS-113: added general order DTOs

---
+++ src/Dto/Order/Order/Get.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Copyright Shopgate Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Shopgate Inc, 804 Congress Ave, Austin, Texas 78701 <interfaces@shopgate.com>
+ * @copyright Shopgate Inc
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+
+namespace Shopgate\ConnectSdk\Dto\Order\Order;
+
+use Shopgate\ConnectSdk\Dto\Order\Order;
+
+/**
+ * @method string getOrderNumber()
+ * @method string getExternalCode()
+ * @method string getType()
+ * @method string getCustomerId()
+ * @method string getExternalCustomerNumber()
+ * @method string getStatus()
+ * @method bool getExpedited()
+ * @method string getLocaleCode()
+ * @method string getCurrencyCode()
+ * @method bool getTaxExempt()
+ * @method object getSpecialInstructions()
+ * @method object getData()
+ * @method string getFulfillmentStatus()
+ * @method int getPrimaryBillToAddressSequenceIndex()
+ * @method int getPrimaryShipToAddressSequenceIndex()
+ * @method object[] getAddressSequences()
+ * @method float getSubTotal()
+ * @method float getDiscountAmount()
+ * @method float getPromoAmount()
+ * @method float getTaxAmount()
+ * @method float getTax2Amount()
+ * @method float getShippingSubTotal()
+ * @method float getShippingDiscountAmount()
+ * @method float getShippingPromoAmount()
+ * @method float getShippingTotal()
+ * @method float getTotal()
+ * @method string getDate()
+ * @method string getSubmitDate()
+ * @method string getAcceptDate()
+ * @method string getCompleteDate()
+ * @method string getSourceDevice()
+ * @method string getSourceIp()
+ * @method object[] getFulfillmentGroups()
+ * @method object[] getLineItems()
+ * @method object[] getHistory()
+ *
+ * @codeCoverageIgnore
+ */
+class Get extends Order
+{
+    /**
+     * @var array
+     */
+    protected $schema = [
+        'type' => 'object',
+        'properties' => [
+            'orderNumber' => ['type' => 'string'],
+            'externalCode' => ['type' => 'string'],
+            'type' => ['type' => 'string'],
+            'customerId' => ['type' => 'string'],
+            'externalCustomerNumber' => ['type' => 'string'],
+            'status' => ['type' => 'string'],
+            'expedited' => ['type' => 'boolean'],
+            'localeCode' => ['type' => 'string'],
+            'currencyCode' => ['type' => 'string'],
+            'taxExempt' => ['type' => 'boolean'],
+            'specialInstructions' => ['type' => 'object'],
+            'data' => ['type' => 'object'],
+            'fulfillmentStatus' => ['type' => 'string'],
+            'primaryBillToAddressSequenceIndex' => ['type' =>'number'],
+            'primaryShipToAddressSequenceIndex' => ['type' => 'number'],
+            'addressSequences' => ['type' => 'array'],
+            'subTotal' => ['type' =>'number'],
+            'discountAmount' => ['type' =>'number'],
+            'promoAmount' => ['type' =>'number'],
+            'taxAmount' => ['type' =>'number'],
+            'tax2Amount' => ['type' =>'number'],
+            'shippingSubTotal' => ['type' =>'number'],
+            'shippingDiscountAmount' => ['type' =>'number'],
+            'shippingPromoAmount' => ['type' =>'number'],
+            'shippingTotal' => ['type' =>'number'],
+            'total' => ['type' =>'number'],
+            'date' => ['type' => 'string'],
+            'submitDate' => ['type' => 'string'],
+            'acceptDate' => ['type' => 'string'],
+            'completeDate' => ['type' => 'string'],
+            'sourceDevice' => ['type' => 'string'],
+            'sourceIp' => ['type' => 'string'],
+            'fulfillmentGroups' => ['type' => 'array'],
+            'lineItems' => ['type' => 'array'],
+            'history' => ['type' => 'array']
+        ],
+        'additionalProperties' => true
+    ];
+}

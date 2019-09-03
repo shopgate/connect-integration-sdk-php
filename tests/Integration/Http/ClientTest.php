8b000e4f170f32a57967a93dcbfeae7c63b8e834
Alexander Wesselburg <info@myportal.de>
2019-09-03 12:19:30 +0200

    MAGENTO-2521 - Fix client integration tests - add invalid token path

--- tests/Integration/Http/ClientTest.php
+++ tests/Integration/Http/ClientTest.php
@@ -147,6 +147,18 @@ class ClientTest extends ShopgateSdkTest
         );
 
         return [
+            'wrong path'                 => [
+                TokenPersistenceException::class,
+                ShopgateSdk::REQUEST_TYPE_DIRECT,
+                getenv('clientId'),
+                getenv('clientSecret'),
+                getenv('merchantCode'),
+                getenv('username'),
+                getenv('password'),
+                getenv('baseUri') ?: '',
+                getenv('env') ?: '',
+                '/invalid/test.txt',
+            ],
             'wrong clientId' => [
                 AuthenticationInvalidException::class,
                 ShopgateSdk::REQUEST_TYPE_DIRECT,

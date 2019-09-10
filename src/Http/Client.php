d2c769cc1c5b4579b5155b775e04f636144c5987
Oliver Müller <oliver.mueller@shopgate.com>
2019-09-10 11:25:50 +0200

    MAGENTO-2508: fixed json_decode of http response body

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -292,7 +292,7 @@ class Client implements ClientInterface
     private function checkForErrorsInResponse($response)
     {
         if ($body = $response->getBody()) {
-            $responseContent = \json_decode($body->read($body->getSize()), true);
+            $responseContent = json_decode((string)$body, true);
 
             if (!isset($responseContent['errors']) || empty($responseContent['errors'])) {
                 return;

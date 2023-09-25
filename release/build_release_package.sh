5a9fb2cee513c139943e9202f06ee77cb5e39748
Timo Ebel <timo.ebel@shopgate.com>
2023-09-25 17:08:38 +0200

    Proposed fix for building ZIP files in CI.

--- release/build_release_package.sh
+++ release/build_release_package.sh
@@ -6,3 +6,4 @@ composer install -vvv --no-dev --ignore-platform-reqs
 rsync -av --exclude-from './release/exclude-filelist.txt' ./ release/shopgate-connect-integration-sdk-php
 cd release
 zip -r ../shopgate-connect-integration-sdk-php.zip shopgate-connect-integration-sdk-php
+cd ..
